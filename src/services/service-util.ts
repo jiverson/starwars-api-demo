63184b090d54e5adbaf6441fa95be4a40ae578cc
jiverson <jiverson222@gmail.com>
2018-06-19 22:03:18 -0700

    update

--- src/services/service-util.ts
+++ src/services/service-util.ts
@@ -1,7 +1,7 @@
-import {Observable} from 'rxjs'
+import { Observable } from 'rxjs'
 
 const toJson = response => response.json()
 
 export function toObservable<T>(fn: Promise<Response>): Observable<T> {
-  return Observable.from(fn).mergeMap(toJson)
+    return Observable.from(fn).mergeMap(toJson)
 }
